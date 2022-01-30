daf5b2ef3eca22d4910e2a2d7b405b42c20ffb7f
Itsqy <syatriarifqi@gmail.com>
2022-01-30 09:18:54 +0700

    multimedia

---
+++ app/src/main/java/com/example/multimedia/VideoStreamingActivity.kt
@@ -0,0 +1,106 @@
+package com.example.multimedia
+
+import android.app.AlertDialog
+import android.content.Context
+import android.graphics.Color
+import android.net.Uri
+import android.os.Bundle
+import android.view.Gravity
+import android.view.ViewGroup
+import android.view.WindowManager
+import android.widget.LinearLayout
+import android.widget.MediaController
+import android.widget.ProgressBar
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import com.example.multimedia.databinding.ActivityVideoStreamingBinding
+
+class VideoStreamingActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityVideoStreamingBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityVideoStreamingBinding.inflate(layoutInflater)
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN
+        )
+
+        setContentView(binding.root)
+
+        supportActionBar?.hide()
+
+        val uriYt = "https://bit.ly/3FHUppV"
+        val controller = MediaController(this)
+        controller.setAnchorView(binding.videoStreamingView)
+
+
+//        val loading = ProgressDialog(this)
+//        loading.setMessage("loading video streaming ..")
+//        loading.show()
+
+        val dialog = setProgressDialog(this, "Loading..")
+        dialog.show()
+
+
+        binding.videoStreamingView.setVideoURI(Uri.parse(uriYt))
+
+        binding.videoStreamingView.requestFocus()
+        binding.videoStreamingView.setOnPreparedListener {
+            it.start()
+            dialog.dismiss()
+        }
+
+
+    }
+
+
+    fun setProgressDialog(context: Context, message: String): AlertDialog {
+        val llPadding = 30
+        val ll = LinearLayout(context)
+        ll.orientation = LinearLayout.HORIZONTAL
+        ll.setPadding(llPadding, llPadding, llPadding, llPadding)
+        ll.gravity = Gravity.CENTER
+        var llParam = LinearLayout.LayoutParams(
+            LinearLayout.LayoutParams.WRAP_CONTENT,
+            LinearLayout.LayoutParams.WRAP_CONTENT
+        )
+        llParam.gravity = Gravity.CENTER
+        ll.layoutParams = llParam
+
+        val progressBar = ProgressBar(context)
+        progressBar.isIndeterminate = true
+        progressBar.setPadding(0, 0, llPadding, 0)
+        progressBar.layoutParams = llParam
+
+        llParam = LinearLayout.LayoutParams(
+            ViewGroup.LayoutParams.WRAP_CONTENT,
+            ViewGroup.LayoutParams.WRAP_CONTENT
+        )
+        llParam.gravity = Gravity.CENTER
+        val tvText = TextView(context)
+        tvText.text = message
+        tvText.setTextColor(Color.parseColor("#000000"))
+        tvText.textSize = 20.toFloat()
+        tvText.layoutParams = llParam
+
+        ll.addView(progressBar)
+        ll.addView(tvText)
+
+        val builder = AlertDialog.Builder(context)
+        builder.setCancelable(true)
+        builder.setView(ll)
+
+        val dialog = builder.create()
+        val window = dialog.window
+        if (window != null) {
+            val layoutParams = WindowManager.LayoutParams()
+            layoutParams.copyFrom(dialog.window?.attributes)
+            layoutParams.width = LinearLayout.LayoutParams.WRAP_CONTENT
+            layoutParams.height = LinearLayout.LayoutParams.WRAP_CONTENT
+            dialog.window?.attributes = layoutParams
+        }
+        return dialog
+    }
+}
